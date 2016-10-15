37d42cc357af935c73e96555af23380233adb5a8
Boyan Yankov <boian.iankov@gmail.com>
2016-10-15 17:33:05 +0300

    updated playground contents - testing Logger

--- Demo/SimpleLoggerDemo.playground/Contents.swift
+++ Demo/SimpleLoggerDemo.playground/Contents.swift
@@ -3,3 +3,15 @@
 import UIKit
 
 var str = "Hello, playground"
+
+Logger.enableLogging(true)
+Logger.useVerbosity(.full)
+
+Logger.general.message(str)
+Logger.debug.message(str)
+Logger.success.message(str)
+Logger.warning.message(str)
+Logger.error.message(str)
+Logger.fatal.message(str)
+Logger.network.message(str)
+Logger.cache.message(str)
