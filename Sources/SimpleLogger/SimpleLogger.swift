a34f3be2deb66ace38c9152c2ffa3e16e25b8745
Boyan Yankov <boyankov@yahoo.com>
2018-07-26 21:37:11 +0300

    updated `SimpleLogger.swift` - changed logging on linux

--- Sources/SimpleLogger/SimpleLogger.swift
+++ Sources/SimpleLogger/SimpleLogger.swift
@@ -158,13 +158,7 @@ public enum SimpleLogger: String {
     @discardableResult
     fileprivate func log(_ any: Any?) -> Logger {
         #if os(Linux)
-        
-        guard let valid_object: AnyObject = any as? AnyObject else {
-            return self
-        }
-        let pointer: UnsafeMutableRawPointer = Unmanaged.passUnretained(valid_object).toOpaque()
-        debugPrint(pointer, terminator: "\n")
-        debugPrint(valid_object, terminator: "\n\n")
+        debugPrint(any, terminator: "\n\n")
         return self
         
         #else
