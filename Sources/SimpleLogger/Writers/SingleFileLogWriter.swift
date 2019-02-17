d407dedee60100b060d6358f0c402b9716b3c281
boyankov <boyan.yankov@yahoo.com>
2019-02-17 16:23:54 +0200

    updated `SingleFileLogWriter.swift` - using writer utils for internal business

--- Sources/SimpleLogger/Writers/SingleFileLogWriter.swift
+++ Sources/SimpleLogger/Writers/SingleFileLogWriter.swift
@@ -19,11 +19,10 @@ struct SingleFileLogWriterImpl: SingleFileLogWriter {
     fileprivate static var logsDirectoryPath: String = ""
     static func update_logsDirectoryPath(_ newValue: String) {
         SingleFileLogWriterImpl.logsDirectoryPath = newValue
-        
         guard SingleFileLogWriterImpl.logsDirectoryPath.count > 0 else {
             return
         }
-        SingleFileLogWriterImpl.createLogsDirecotry(at: self.logsDirectoryPath)
+        SingleFileLogWriterImpl.createLogsDirectory(at: self.logsDirectoryPath)
     }
     
     fileprivate static var logFileName: String = Constants.logFileDefaultName
@@ -46,19 +45,7 @@ struct SingleFileLogWriterImpl: SingleFileLogWriter {
     
     // MARK: - Utils
     static func currentDirectoryPath(from candidate: String) -> String? {
-        guard let valid_url = URL(string: candidate) else {
-            return nil
-        }
-        let pathComponents: [String] = valid_url.pathComponents
-        let folderPath: String
-        let fm = FileManager.default
-        if fm.fileExists(atPath: valid_url.absoluteString) {
-            folderPath = "/\(pathComponents[1..<pathComponents.count-1].joined(separator: "/"))"
-        }
-        else {
-            folderPath = "/\(pathComponents[1..<pathComponents.count].joined(separator: "/"))"
-        }
-        return folderPath
+        return WriterUtils.directoryPath(from: candidate)
     }
     
     static func writeToFile(_ candidate: String) {
@@ -72,55 +59,15 @@ struct SingleFileLogWriterImpl: SingleFileLogWriter {
             print(message)
             return
         }
-        let fm = FileManager.default
-        if !fm.fileExists(atPath: valid_logFilePath) {
-            do {
-                try "".write(toFile: valid_logFilePath, atomically: true, encoding: String.Encoding.utf8)
-            }
-            catch {
-                print("error:\(error)")
-            }
-        }
-        guard fm.isWritableFile(atPath: valid_logFilePath) else {
-            return
-        }
-        guard let valid_fileHandle = FileHandle(forWritingAtPath: valid_logFilePath) else {
-            let message: String = "Unable to obtain valid \(String(describing: FileHandle.self)) object!"
-            print(message)
-            return
-        }
-        let stringToWrite = "\(candidate)\n"
-        valid_fileHandle.seekToEndOfFile()
-        
-        guard let valid_data: Data = stringToWrite.data(using: String.Encoding.utf8) else {
-            let message: String = "Unable to obtain valid \(String(describing: Data.self)) object from candidate=\(stringToWrite)!"
-            print(message)
-            return
-        }
-        valid_fileHandle.write(valid_data)
-        valid_fileHandle.closeFile()
-        
+        WriterUtils.write(candidate, toFileAtPath: valid_logFilePath)
         SingleFileLogWriterImpl.cleanUpIfNeeded()
     }
     
-    fileprivate static func createLogsDirecotry(at path: String) {
+    fileprivate static func createLogsDirectory(at path: String) {
         guard !SingleFileLogWriterImpl.didCreateLogsDirectory else {
             return
         }
-        
-        let fm: FileManager = FileManager.default
-        if !fm.fileExists(atPath: path) {
-            do {
-                try fm.createDirectory(atPath: path, withIntermediateDirectories: false, attributes: nil)
-                SingleFileLogWriterImpl.didCreateLogsDirectory = true
-            }
-            catch {
-                print("error:\(error)")
-            }
-        }
-        else {
-            SingleFileLogWriterImpl.didCreateLogsDirectory = true
-        }
+        self.didCreateLogsDirectory = WriterUtils.createDirectory(at: path)
     }
     
     fileprivate static func logFilePath() -> String? {
@@ -134,39 +81,18 @@ struct SingleFileLogWriterImpl: SingleFileLogWriter {
         guard let valid_logFilePath: String = SingleFileLogWriterImpl.logFilePath() else {
             return
         }
-        let fm = FileManager.default
-        guard fm.fileExists(atPath: valid_logFilePath) else {
-            return
-        }
-        let fileSize: UInt64 = SingleFileLogWriterImpl.fileSize(at: valid_logFilePath)
-        
         guard SingleFileLogWriterImpl.logFileMaxSizeInBytes > 0 else {
             return
         }
+        let fileSize: UInt64 = SingleFileLogWriterImpl.fileSize(at: valid_logFilePath)
         guard fileSize > SingleFileLogWriterImpl.logFileMaxSizeInBytes else {
             return
         }
-        
-        do {
-            try fm.removeItem(atPath: valid_logFilePath)
-        }
-        catch {
-            print("error:\(error)")
-        }
+        WriterUtils.removeFile(at: valid_logFilePath)
     }
     
     fileprivate static func fileSize(at path: String) -> UInt64 {
-        let fm: FileManager = FileManager.default
-        guard fm.fileExists(atPath: path) else {
-            return 0
-        }
-        guard let valid_attributes: [FileAttributeKey: Any] = try? fm.attributesOfItem(atPath: path) else {
-            return 0
-        }
-        guard let valid_fileSize: UInt64 = valid_attributes[FileAttributeKey.size] as? UInt64 else {
-            return 0
-        }
-        return valid_fileSize
+        return WriterUtils.fileSize(at: path)
     }
 }
 
