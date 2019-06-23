6490ff980c4304ae1e6d48bc2f4eddc0a2df2590
boyankov <boyan.yankov@yahoo.com>
2019-06-23 21:58:59 +0300

    added `SimpleLogger.h`

---
+++ SimpleLogger.h
@@ -0,0 +1,16 @@
+//
+//  SimpleLogger.h
+//  SimpleLogger
+//
+//  Created by Boyan Yankov on W25 23/Jun/2019 Sun.
+//
+//  Copyright (c) 2019 boyankov <boyankov@yahoo.com>
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SimpleLogger.
+FOUNDATION_EXPORT double SimpleLoggerVersionNumber;
+
+//! Project version string for SimpleLogger.
+FOUNDATION_EXPORT const unsigned char SimpleLoggerVersionString[];
